6479dd937312e571b286fdf337755bf7bce68655
woodwardmw <mark@mark-laptop.home>
2021-09-30 21:59:41 +0100

    Changes for Audible Fall Harvest Sale Sept 2021. Some new untested changes too, tidying up the code, but shouldn't be difficult to undo.

--- add_new_linked_urls.py
+++ add_new_linked_urls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-book_file = 'Audible 2 for 1 Sept 2021 - 2 for 1 Sept 2021.csv'
+book_file = 'Fall Harvest Sale Sept 2021.csv'
 linked_URLs = pd.read_csv(os.getcwd() + '/' + 'Linked_URLs.csv')
 book_df = pd.read_csv(os.getcwd() + '/' + book_file)
 
@@ -9,10 +9,12 @@ def add_new_linked_urls(linked_URLs, book_database):
     book_database.dropna(axis = 0, subset = ['Goodreads_Link'], inplace = True)
 
     for index, item in book_database.iterrows():
-        if index < 10:
+        if True:
             print(f'Index: {index}')
             title = item['Audible_Title']
+            # print(title)
             author = item['Audible_Author']
+            # print(author)
             goodreads_link = item['Goodreads_Link']
             mask = (linked_URLs['Audible_Title'] == title) & (linked_URLs['Audible_Author'] == author)
             print(linked_URLs[mask])
@@ -20,7 +22,7 @@ def add_new_linked_urls(linked_URLs, book_database):
                 print('Not in Linked URLs')
                 print({'Audible_Title': title, 'Audible_Author': author, 'Goodreads_Link': goodreads_link})
                 linked_URLs = linked_URLs.append({'Audible_Title': title, 'Audible_Author': author, 'Goodreads_Link': goodreads_link}, ignore_index=True)
-                print(linked_URLs)
+                # print(linked_URLs)
             else:
                 print('Already in Linked URLs')
                 continue
--- get_goodreads_ratings.py
+++ get_goodreads_ratings.py
@@ -17,40 +17,54 @@ from goodreads import getRating, getRatingFromRow, getGoodreadsMatches
 from audible import audibleLogin, getCategories, getAudibleDataForCategory
 from amazon import amazonLogin, getAmazonMatches
 
-base_url = 'https://www.audible.com/ep/eureka-sale?ref=a_ep_eureka_c5_tab1&pf_rd_p=418e7020-0b96-4cbc-919e-79c851f139ff&pf_rd_r=8BHDCJ5N7RHGQF8F4ZNF'
-book_file = 'Eureka Sale Sept 2021.csv'  # Can be just the Audible list, or an already processed merge of Audible and Goodreads Data
+base_url = 'https://www.audible.com/special-promo/2for1/cat?node=23435846011&pf_rd_p=c32b7277-122c-4c55-81e7-37c155004f75&pf_rd_r=25WW3MMD4KQNYQ0K54G0'
+book_file = 'Fall Harvest Sale Sept 2021.csv'  # Can be just the Audible list, or an already processed merge of Audible and Goodreads Data
 
-# web = audibleLogin()
-# categorySelect = 'body > div.adbl-page > div.adbl-main > div#center-5 > div > div > div.bc-box > div.bc-color-secondary > div.bc-container > div.bc-spacing-mini > div.bc-col-responsive'
+getAudibleData = False
+getGoodreadsRatings = True
+getAmazonLinks = False
 
-# categories = getCategories('All Titles', base_url, web, categorySelect)
-# book_df = pd.DataFrame({'Audible_Title':pd.Series([], dtype='str'), 'Audible_Subtitle':pd.Series([], dtype='str'), 'Audible_Author':pd.Series([], dtype='str'), 'Audible_Link':pd.Series([], dtype='str'), 'Image_Link':pd.Series([], dtype='str'), 'Audible_Category':pd.Series([], dtype='str'), 'Goodreads_Link':pd.Series([], dtype='str'), 'Amazon_Link':pd.Series([], dtype='str'), 'Goodreads_Rating':pd.Series([], dtype='float'), 'Number_of_Ratings':pd.Series([], dtype='int')})
+categorySelect = 'body > div.adbl-page > div.adbl-main > div.bc-container > div > div > div#left-1 > form > div.categories > span > ul'
+bookItemSelect = 'body > div.adbl-page > div.adbl-main > div.bc-container > div > div.bc-col-9 > div#center-3 > div > div > span > ul > div.adbl-impression-container > li.productListItem'  # to end in li
+imageDivSelect = 'div > div.bc-col-8 > div > div.bc-col-5 > div > div.bc-col-12 > div'
+textDivSelect1 = 'div > div.bc-col-8 > div > div.bc-col-6 > div > div.bc-col-12 > span > ul'
+MAX_PAGES = 5
+
+
+
+if getAudibleData == True:
+    web = audibleLogin()
+    categories = getCategories('All Titles', base_url, web, categorySelect)
+    book_df = pd.DataFrame({'Audible_Title':pd.Series([], dtype='str'), 'Audible_Subtitle':pd.Series([], dtype='str'), 'Audible_Author':pd.Series([], dtype='str'), 'Audible_Link':pd.Series([], dtype='str'), 'Image_Link':pd.Series([], dtype='str'), 'Audible_Category':pd.Series([], dtype='str'), 'Goodreads_Link':pd.Series([], dtype='str'), 'Amazon_Link':pd.Series([], dtype='str'), 'Goodreads_Rating':pd.Series([], dtype='float'), 'Number_of_Ratings':pd.Series([], dtype='int')})
+    for _, (category, audibleurl) in enumerate(categories.items()):  # I think this could be  for category, audibleurl in categories.items():
+        if category != 'All Titles':
+            book_df = getAudibleDataForCategory(audibleurl, category, MAX_PAGES, book_df, web, bookItemSelect, imageDivSelect, textDivSelect1)
+
+    # Need to remove duplicates (by title and author) as these mess up the goodreads matching
+    book_df.to_csv(os.getcwd() + '/' + book_file)
+
+
+
+if getGoodreadsRatings == True:
+    linked_URLs = pd.read_csv(os.getcwd() + '/Linked_URLs.csv')
+    book_df = pd.read_csv(os.getcwd() + '/' + book_file)
+    goodreads_df = getGoodreadsMatches(book_df, linked_URLs, refresh_existing = True)
+    book_df.update(goodreads_df)    
+    book_df.to_csv(os.getcwd() + '/' + book_file, index = False)
+
+
+if getAmazonLinks == True:
+    linked_URLs = pd.read_csv(os.getcwd() + '/Linked_URLs.csv')
+    book_df = pd.read_csv(os.getcwd() + '/' + book_file)
+    web=amazonLogin()
+    getAmazonMatches(book_df, linked_URLs, web, refresh_existing = False)
+    book_df.to_csv(os.getcwd() + '/' + book_file, index = False)
+
+
 # book_df = pd.read_csv(os.getcwd() + '/' + book_file)
-# max_pages = 5
-# bookItemSelect = 'body > div.adbl-page > div.adbl-main > div#center-10 > div > div > span > ul > div.adbl-impression-container > li.productListItem'  # to end in li
-# imageDivSelect = 'div > div.bc-col-9 > div > div.bc-col-4 > div > div.bc-col-12 > div'
-# textDivSelect1 = 'div > div.bc-col-9 > div > div.bc-col-7 > div > div.bc-col-12 > span > ul'
+# book_df = book_df.astype({'Amazon_Link': str})
 
 
-# for _, (category, audibleurl) in enumerate(categories.items()):
-#     if category != 'All Titles':
-#         book_df = getAudibleDataForCategory(audibleurl, category, max_pages, book_df, web, bookItemSelect, imageDivSelect, textDivSelect1)
-
-# Need to remove duplicates (by title and author) as these mess up the goodreads matching
-
-
-# book_df.to_csv(os.getcwd() + '/' + book_file)
-
-web=amazonLogin()
-
-book_df = pd.read_csv(os.getcwd() + '/' + book_file)
-book_df = book_df.astype({'Amazon_Link': str})
-linked_URLs = pd.read_csv(os.getcwd() + '/Linked_URLs.csv')
-
-# goodreads_df = getGoodreadsMatches(book_df, linked_URLs, refresh_existing = False)
-# print(goodreads_df.head(20))
-# book_df.update(goodreads_df)
-book_df = getAmazonMatches(book_df, linked_URLs, web)
 
 
 book_df.to_csv(os.getcwd() + '/' + book_file, index = False)
